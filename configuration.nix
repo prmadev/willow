c99c84d7ad6c6147f2c5af000ff271b299242320
amirography <amirhossein.alesheikh@gmail.com>
2022-07-11 14:24:25 +0430

    auto: syncing

--- configuration.nix
+++ configuration.nix
@@ -164,4 +164,16 @@
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
   ];
+  fonts = {
+	  fontconfig = {
+		  defaultFonts = {
+			  serif = ["recursive" "JetBrainsMono Nerd Font" "vazir"];
+			  sansSerif = ["recursive" "JetBrainsMono Nerd Font" "vazir"];
+			  monospace = ["recursive" "JetBrainsMono Nerd Font" "vazir"];
+
+			  };
+
+		  };
+		  
+	  };
 }
