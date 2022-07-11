70a82f51be0dc53458c78fd66854d529ffcff44e
amirography <amirhossein.alesheikh@gmail.com>
2022-07-11 20:55:03 +0430

    auto: syncing

--- home/modules/terminal/kitty/default.nix
+++ home/modules/terminal/kitty/default.nix
@@ -9,6 +9,16 @@
       package = pkgs.recursive;
       name = "Recursive Sans Linear";
       size = 10;
+      settings = {
+        scrollback_lines = 15000;
+        enable_audio_bell = false;
+        update_check_interval = 0;
+        repaint_delay = 10;
+        input_delay = 1;
+        sync_to_monitor = true;
+        allow_remote_control = true;
+      };
+      theme = "Rosé Pine";
     };
   };
 }
