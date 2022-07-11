4710a2d790385918a9874fddf32ec883d8c0f39a
amirography <amirhossein.alesheikh@gmail.com>
2022-07-11 19:21:12 +0430

    auto: syncing

--- home/modules/environment/default.nix
+++ home/modules/environment/default.nix
@@ -5,9 +5,9 @@
   services.gpg-agent.enable = true;
   fonts.fontconfig.enable = true;
   gtk.enable = true;
-  gtk.font.name = "Recursive";
-  gtk.font.package = pkgs.recursive;
-  gtk.font.size = 10;
+  # gtk.font.name = "Recursive";
+  # gtk.font.package = pkgs.recursive;
+  # gtk.font.size = 10;
   home.keyboard.layout = "us,fa";
   home.keyboard.options = ["grp:alt_shift_toggle"];
 }
