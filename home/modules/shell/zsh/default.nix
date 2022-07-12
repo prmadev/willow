2010e346ed811c97075501070c754bb03199910c
amirography <amirhossein.alesheikh@gmail.com>
2022-07-12 10:49:00 +0430

    auto: syncing

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -1,23 +1,23 @@
 {pkgs, ...}: {
   programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableSyntaxHighlighting = true;
-  programs.zsh.enableVteIntegration = true;
-  programs.zsh.autocd = true;
-  programs.zsh.oh-my-zsh.enable = true;
+  # programs.zsh.enableAutosuggestions = true;
+  # programs.zsh.enableCompletion = true;
+  # programs.zsh.enableSyntaxHighlighting = true;
+  # programs.zsh.enableVteIntegration = true;
+  # programs.zsh.autocd = true;
+  # programs.zsh.oh-my-zsh.enable = true;
 
   # programs.broot.enableZshIntegration = true;
 
-  programs.fzf.enableZshIntegration = true;
-
-  programs.just.enableZshIntegration = true;
-
-  programs.nix-index.enableZshIntegration = true;
-
-  programs.starship.enableZshIntegration = true;
-
-  programs.zoxide.enableZshIntegration = true;
-
-  services.gpg-agent.enableZshIntegration = true;
+  # programs.fzf.enableZshIntegration = true;
+  #
+  # programs.just.enableZshIntegration = true;
+  #
+  # programs.nix-index.enableZshIntegration = true;
+  #
+  # programs.starship.enableZshIntegration = true;
+  #
+  # programs.zoxide.enableZshIntegration = true;
+  #
+  # services.gpg-agent.enableZshIntegration = true;
 }
