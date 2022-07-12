0b5718cf71706a87ff6a5a86bfd81d11159e0086
amirography <amirhossein.alesheikh@gmail.com>
2022-07-12 13:43:48 +0430

    auto: syncing

--- home/modules/shell/default.nix
+++ home/modules/shell/default.nix
@@ -1,32 +1,32 @@
 {pkgs, ...}: {
   imports = [
-    # ./zsh
+    ./zsh
     ./bash
     ./nu
   ];
 
-  programs.info.enable = true;
-  programs.tealdeer.enable = true;
-  programs.bat.enable = true;
-  programs.exa.enable = true;
-  programs.exa.enableAliases = true;
-  programs.fzf.enable = true;
-  programs.fzf.changeDirWidgetCommand = "fd --type d";
-  programs.fzf.tmux.enableShellIntegration = true;
-  programs.starship.enable = true;
-  programs.jq.enable = true;
+  # programs.info.enable = true;
+  # programs.tealdeer.enable = true;
+  # programs.bat.enable = true;
+  # programs.exa.enable = true;
+  # programs.exa.enableAliases = true;
+  # programs.fzf.enable = true;
+  # programs.fzf.changeDirWidgetCommand = "fd --type d";
+  # programs.fzf.tmux.enableShellIntegration = true;
+  # programs.starship.enable = true;
+  # programs.jq.enable = true;
+  #
+  # programs.man.generateCaches = true;
+  # programs.pet.enable = true; # TODO
 
-  programs.man.generateCaches = true;
-  programs.pet.enable = true; # TODO
-
-  # home.shellAliases = {
-  #   tree = "exa --tree --icons";
-  #   b = "br";
-  #   rg = "rg --sort path";
-  #   jctl = "journalctl -p 3 -xb";
-  #   df = "df -h";
-  #   go = "richgo";
-  #   sudo = "sudo -E";
-  #   fzf = "fzf --preview='pistol {}'";
-  # };
+  home.shellAliases = {
+    tree = "exa --tree --icons";
+    b = "br";
+    rg = "rg --sort path";
+    jctl = "journalctl -p 3 -xb";
+    df = "df -h";
+    go = "richgo";
+    sudo = "sudo -E";
+    fzf = "fzf --preview='pistol {}'";
+  };
 }
