c3d7038ccebf1eb83f8d7393f50a901483bee37b
amirography <amirhossein.alesheikh@gmail.com>
2022-07-12 10:41:13 +0430

    auto: syncing

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -1,8 +1,8 @@
 {pkgs, ...}: {
   programs.zsh.enable = true;
-  # programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableAutosuggestions = true;
   programs.zsh.enableCompletion = true;
-  programs.zsh.enableSyntaxHighlighting = true;
+  # programs.zsh.enableSyntaxHighlighting = true;
   programs.zsh.enableVteIntegration = true;
   programs.zsh.autocd = true;
   programs.zsh.oh-my-zsh.enable = true;
