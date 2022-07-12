f6468857017b7ed795ade5d7420fcebe37e818a4
amirography <amirhossein.alesheikh@gmail.com>
2022-07-12 12:46:43 +0430

    auto: syncing

--- home/modules/shell/bash/default.nix
+++ home/modules/shell/bash/default.nix
@@ -1,6 +1,13 @@
 {pkgs, ...}: {
+  programs.bash.enable = true;
+  programs.bash = {
+    enable = true;
+    enableVteIntegration = true;
+  };
   programs.starship.enableBashIntegration = true;
   programs.fzf.enableBashIntegration = true;
   programs.zoxide.enableBashIntegration = true;
   programs.just.enableBashIntegration = true;
+  programs.nix-index.enableBashIntegration = true;
+  programs.gpg-agent.enableBashIntegration = true;
 }
