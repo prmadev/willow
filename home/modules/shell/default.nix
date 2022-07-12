d7fea87c643d24a0d1ce719fe9e3ed50fb3ea13c
amirography <amirhossein.alesheikh@gmail.com>
2022-07-12 13:45:07 +0430

    auto: syncing

--- home/modules/shell/default.nix
+++ home/modules/shell/default.nix
@@ -1,23 +1,23 @@
 {pkgs, ...}: {
   imports = [
     ./zsh
-    ./bash
+    # ./bash
     ./nu
   ];
 
-  # programs.info.enable = true;
-  # programs.tealdeer.enable = true;
-  # programs.bat.enable = true;
-  # programs.exa.enable = true;
-  # programs.exa.enableAliases = true;
-  # programs.fzf.enable = true;
-  # programs.fzf.changeDirWidgetCommand = "fd --type d";
-  # programs.fzf.tmux.enableShellIntegration = true;
-  # programs.starship.enable = true;
-  # programs.jq.enable = true;
-  #
-  # programs.man.generateCaches = true;
-  # programs.pet.enable = true; # TODO
+  programs.info.enable = true;
+  programs.tealdeer.enable = true;
+  programs.bat.enable = true;
+  programs.exa.enable = true;
+  programs.exa.enableAliases = true;
+  programs.fzf.enable = true;
+  programs.fzf.changeDirWidgetCommand = "fd --type d";
+  programs.fzf.tmux.enableShellIntegration = true;
+  programs.starship.enable = true;
+  programs.jq.enable = true;
+
+  programs.man.generateCaches = true;
+  programs.pet.enable = true; # TODO
 
   home.shellAliases = {
     tree = "exa --tree --icons";
