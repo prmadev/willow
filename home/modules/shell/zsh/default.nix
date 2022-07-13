2dd349c641dde6555d3fbc27282ad5b1da06779b
amirography <amirhossein.alesheikh@gmail.com>
2022-07-13 19:30:25 +0430

    auto: syncing

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -1,78 +1,82 @@
 {pkgs, ...}: {
-	programs.zsh = {
-  enable = true;
-  enableAutosuggestions = true;
-  enableCompletion = true;
-  enableSyntaxHighlighting = true;
-  enableVteIntegration = true;
-  autocd = true;
-  dotDir = ".config/zsh";
-  history.expireDuplicatesFirst = true;
-  history.extended = true;
-  history.ignoreDups = true;
-  history.save = 3000000;
-  history.size = 30000000;
+  programs.zsh = {
+    enable = true;
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    enableSyntaxHighlighting = true;
+    enableVteIntegration = true;
+    autocd = true;
+    dotDir = ".config/zsh";
 
-  #prezto.enable = true;
-  oh-my-zsh.enable = true;
-  oh-my-zsh.extraConfig = ''
-    HYPHEN_INSENSITIVE="true"
-    ENABLE_CORRECTION="true"
-  '';
+    history.expireDuplicatesFirst = true;
+    history.extended = true;
+    history.ignoreDups = true;
+    history.save = 3000000;
+    history.size = 30000000;
 
-  oh-my-zsh.plugins = [
-    "colored-man-pages"
-    "colorize"
-    "compleat"
-    # "colors"
+    #prezto.enable = true;
+	#
+	oh-my-zsh = {
+    enable = true;
+    extraConfig = ''
+      HYPHEN_INSENSITIVE="true"
+      ENABLE_CORRECTION="true"
+    '';
 
-    "fzf"
-    # "fzf-zsh-completions"
-    # "fzf-marks"
-    # "fzf-finder"
-    # "fzf-zsh-plugin"
-    # "fzf-tab"
-    #
-    "git-lfs"
-    "gitfast"
-    "git-extras"
-    "github"
-    "gh"
-    "gitignore"
-    "golang"
-    "gpg-agent"
-    # "globalias"
-    "history"
-    "history-substring-search"
-    "keychain"
-    "man"
-    "npm"
-    "per-directory-history"
-    "pip"
-    "python"
-    "repo"
-    "rsync"
-    "rbw"
-    "ripgrep"
-    "rust"
-    "ssh-agent"
-    "shell-proxy"
-    "systemd"
-    "taskwarrior"
-    # "thefuck"
-    "transfer"
-    "universalarchive"
-    "urltools"
-    "web-search"
-    "zoxide"
-    "zsh-interactive-cd"
-    # "zsh-autosuggestions"
-    # "zsh-bash-completions-fallback"
-    # "completion"
-    # "fast-syntax-highlighting"
-    # "zsh-syntax-highlighting"
-  ];
+    plugins = [
+      "colored-man-pages"
+      "colorize"
+      "compleat"
+      # "colors"
+
+      "fzf"
+      # "fzf-zsh-completions"
+      # "fzf-marks"
+      # "fzf-finder"
+      # "fzf-zsh-plugin"
+      # "fzf-tab"
+      #
+      "git-lfs"
+      "gitfast"
+      "git-extras"
+      "github"
+      "gh"
+      "gitignore"
+      "golang"
+      "gpg-agent"
+      # "globalias"
+      "history"
+      "history-substring-search"
+      "keychain"
+      "man"
+      "npm"
+      "per-directory-history"
+      "pip"
+      "python"
+      "repo"
+      "rsync"
+      "rbw"
+      "ripgrep"
+      "rust"
+      "ssh-agent"
+      "shell-proxy"
+      "systemd"
+      "taskwarrior"
+      # "thefuck"
+      "transfer"
+      "universalarchive"
+      "urltools"
+      "web-search"
+      "zoxide"
+      "zsh-interactive-cd"
+      # "zsh-autosuggestions"
+      # "zsh-bash-completions-fallback"
+      # "completion"
+      # "fast-syntax-highlighting"
+      # "zsh-syntax-highlighting"
+    ];
 };
+  };
   # programs.broot.enableZshIntegration = true;
   #
   programs.fzf.enableZshIntegration = true;
