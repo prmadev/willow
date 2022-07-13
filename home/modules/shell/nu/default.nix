64ea41e9d6a6eba0286ca05f4214dc262c116bbe
amirography <amirhossein.alesheikh@gmail.com>
2022-07-13 18:38:25 +0430

    auto: syncing

--- home/modules/shell/nu/default.nix
+++ home/modules/shell/nu/default.nix
@@ -1,5 +1,11 @@
 {pkgs, ...}: {
   programs.nushell = {
     enable = true;
+    envFile.text = ''
+      starship init nu | save ~/.cache/starship/init.nu
+    '';
+    configFile.text = ''
+      source ~/.cache/starship/init.nu
+    '';
   };
 }
