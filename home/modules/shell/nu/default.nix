8bfff130a5777b5e37c10163c7d7fe2f182682a1
amirography <amirhossein.alesheikh@gmail.com>
2022-07-13 18:55:38 +0430

    auto: syncing

--- home/modules/shell/nu/default.nix
+++ home/modules/shell/nu/default.nix
@@ -5,7 +5,12 @@
       starship init nu | save ~/.cache/starship/init.nu
     '';
     configFile.text = ''
-          source ~/.cache/starship/init.nu
+      let $config = {
+           filesize_metric: false
+           table_mode: rounded
+           use_ls_colors: true
+         }
+           source ~/.cache/starship/init.nu
     '';
   };
 }
