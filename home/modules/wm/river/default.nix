85562b5ebe203398d595fcf4ee43d9c6a39d0490
amirography <amirhossein.alesheikh@gmail.com>
2022-07-13 17:14:33 +0430

    auto: syncing

--- home/modules/wm/river/default.nix
+++ home/modules/wm/river/default.nix
@@ -7,19 +7,19 @@
     river # my favorite window manager in wayland
   ]; #TODO make a more integrated configuration
 
-  systemd.user.services.river = {
-    Unit = {
-      Description = "River daemon";
-      After = "network.target";
-    };
-
-    Service = {
-      Type = "simple";
-      Restart = "always";
-      ExecStart = "${pkgs.river}/bin/river";
-    };
-    Install = {
-      WantedBy = ["multi-user.target"];
-    };
-  };
+  # systemd.user.services.river = {
+  #   Unit = {
+  #     Description = "River daemon";
+  #     After = "network.target";
+  #   };
+  #
+  #   Service = {
+  #     Type = "simple";
+  #     Restart = "always";
+  #     ExecStart = "${pkgs.river}/bin/river";
+  #   };
+  #   Install = {
+  #     WantedBy = ["multi-user.target"];
+  #   };
+  # };
 }
