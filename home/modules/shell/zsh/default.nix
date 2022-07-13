23e49e6c51c9198f8eab27c6cdf2348239f73b33
amirography <amirhossein.alesheikh@gmail.com>
2022-07-13 19:13:31 +0430

    auto: syncing

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -1,15 +1,15 @@
 {pkgs, ...}: {
   programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableSyntaxHighlighting = true;
-  programs.zsh.enableVteIntegration = true;
-  programs.zsh.autocd = true;
-  programs.zsh.oh-my-zsh.enable = true;
+  # programs.zsh.enableAutosuggestions = true;
+  # programs.zsh.enableCompletion = true;
+  # programs.zsh.enableSyntaxHighlighting = true;
+  # programs.zsh.enableVteIntegration = true;
+  # programs.zsh.autocd = true;
+  # programs.zsh.oh-my-zsh.enable = true;
   # programs.zsh.prezto.enable = true;
   # programs.zsh.completionInit = "";
-  programs.zsh.history.expireDuplicatesFirst = true;
-  programs.zsh.history.extended = true;
+  # programs.zsh.history.expireDuplicatesFirst = true;
+  # programs.zsh.history.extended = true;
 
   # programs.zsh.history.ignoreDups = true;
   # programs.zsh.dotDir = ".config/zsh";
@@ -17,10 +17,10 @@
   # programs.zsh.history.save = 30000;
   # programs.zsh.history.size = 30000;
 
-  programs.zsh.oh-my-zsh.extraConfig = ''
-    HYPHEN_INSENSITIVE="true"
-    ENABLE_CORRECTION="true"
-  '';
+  # programs.zsh.oh-my-zsh.extraConfig = ''
+  #   HYPHEN_INSENSITIVE="true"
+  #   ENABLE_CORRECTION="true"
+  # '';
 
   programs.zsh.oh-my-zsh.plugins = [
     "colored-man-pages"
@@ -76,15 +76,15 @@
   ];
   # programs.broot.enableZshIntegration = true;
 
-  programs.fzf.enableZshIntegration = true;
-
-  programs.just.enableZshIntegration = true;
-
-  programs.nix-index.enableZshIntegration = true;
-
-  programs.starship.enableZshIntegration = true;
-
-  programs.zoxide.enableZshIntegration = true;
-
-  services.gpg-agent.enableZshIntegration = true;
+  # programs.fzf.enableZshIntegration = true;
+  #
+  # programs.just.enableZshIntegration = true;
+  #
+  # programs.nix-index.enableZshIntegration = true;
+  #
+  # programs.starship.enableZshIntegration = true;
+  #
+  # programs.zoxide.enableZshIntegration = true;
+  #
+  # services.gpg-agent.enableZshIntegration = true;
 }
