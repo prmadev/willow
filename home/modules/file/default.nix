857f3a81ec2757bbfeea25c4cdf0a1e4b57e06ef
amirography <amirhossein.alesheikh@gmail.com>
2022-07-15 15:25:57 +0430

    auto: syncing

--- home/modules/file/default.nix
+++ home/modules/file/default.nix
@@ -30,12 +30,14 @@ in {
     previewer.keybinding = "i";
     commands = {
       fzf_search = ''
+	  $\{\{
         res="$( \
             RG_PREFIX="rg --column --line-number --no-heading --color=always \
                 --smart-case "
             FZF_DEFAULT_COMMAND="$RG_PREFIX \'\'" fzf --bind "change:reload:$RG_PREFIX {q} || true"  --ansi --layout=reverse --header 'Search in files' | cut -d':' -f1
         )"
         [ ! -z "$res" ] && lf -remote "send $id select \"$res\""
+	  }}
       '';
       fzf_jump = ''
         res="$(find . -maxdepth 1 | fzf --reverse --header='Jump to location' | sed 's/\\/\\\\/g;s/"/\\"/g')"
