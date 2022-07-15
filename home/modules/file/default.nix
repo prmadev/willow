2f157006ef91d90caeb03b0936a1bc68ffa0db7f
amirography <amirhossein.alesheikh@gmail.com>
2022-07-15 15:38:06 +0430

    auto: syncing

--- home/modules/file/default.nix
+++ home/modules/file/default.nix
@@ -28,11 +28,11 @@ in {
     previewer.keybinding = "i";
     commands = {
       zip = ''
-        set -f \
+        {set -f \
         mkdir $1 \
         cp -r $fx $1 \
         zip -r $1.zip $1 \
-        rm -rf $1
+        rm -rf $1 }
       '';
       tar = ''
         set -f
