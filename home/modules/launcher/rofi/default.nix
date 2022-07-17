23af4e04dfeca3b7bfb55df563b44f841062db37
amirography <amirhossein.alesheikh@gmail.com>
2022-07-17 12:21:14 +0430

    auto: syncing

--- home/modules/launcher/rofi/default.nix
+++ home/modules/launcher/rofi/default.nix
@@ -7,7 +7,7 @@
     package = pkgs.rofi-wayland.override {plugins = [pkgs.rofi-rbw];};
   };
   home.packages = with pkgs; [
-    rofi-rbw # for password management
+    # rofi-rbw # for password management
     # networkmanager_dmenu # needs dmenu to work
   ];
 }
