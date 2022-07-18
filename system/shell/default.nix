881c9ae1ad86454144b6330bbeea3f6159be6217
amirography <amirhossein.alesheikh@gmail.com>
2022-07-18 14:42:48 +0430

    auto: syncing

--- system/shell/default.nix
+++ system/shell/default.nix
@@ -80,6 +80,24 @@
     # zsh
   ];
 
+  console.colors = [
+    "403c58"
+    "ea6f91"
+    "9bced7"
+    "f1ca93"
+    "34738e"
+    "c3a5e6"
+    "eabbb9"
+    "faebd7"
+    "6f6e85"
+    "ea6f91"
+    "9bced7"
+    "f1ca93"
+    "34738e"
+    "c3a5e6"
+    "eabbb9"
+    "ffffff"
+  ];
   services.atd.enable = true;
   services.dbus.enable = true;
   # services.dbus.packages = [pkgs.gcr];
