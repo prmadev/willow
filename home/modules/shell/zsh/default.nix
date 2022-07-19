7a2c6baef00b448e54fef0f57192f54c5a0193ce
amirography <amirhossein.alesheikh@gmail.com>
2022-07-19 08:37:58 +0430

    auto: syncing

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -15,14 +15,14 @@
     };
     autocd = true;
     initExtra = ''
-		if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
-  			exec tmux
-		fi
-      export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
-       --color=fg:#e0def4,bg:#1f1d2e,hl:#6e6a86
-       --color=fg+:#908caa,bg+:#191724,hl+:#908caa
-       --color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7
-       --color=marker:#ebbcba,spinner:#eb6f92,header:#ebbcba"
+      if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
+      			exec tmux
+      fi
+          export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS"
+           --color=fg:#e0def4,bg:#1f1d2e,hl:#6e6a86
+           --color=fg+:#908caa,bg+:#191724,hl+:#908caa
+           --color=info:#9ccfd8,prompt:#f6c177,pointer:#c4a7e7
+           --color=marker:#ebbcba,spinner:#eb6f92,header:#ebbcba"
     ''; # TODO use the module
     # TODO check the old config for extra parameteres
     dotDir = ".config/zsh";
--- home/modules/terminal/tmux/default.nix
+++ home/modules/terminal/tmux/default.nix
@@ -1,10 +1,15 @@
 {pkgs, ...}: {
   programs.tmux = {
     enable = true;
+    keyMode = "vi";
     clock24 = true;
     newSession = true;
     shell = "${pkgs.zsh}/bin/zsh";
     shortcut = "z";
     tmuxp.enable = true;
+	sensibleOnTop = true;
+	extraConfig = ''
+
+	'';
   };
 }
