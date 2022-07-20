6592776f041ce13d7c245217dd92ea3ecffc9d27
amirography <amirhossein.alesheikh@gmail.com>
2022-07-20 11:51:28 +0430

    auto: syncing

--- home/modules/launcher/rofi/default.nix
+++ home/modules/launcher/rofi/default.nix
@@ -1,13 +1,31 @@
-{pkgs, ...}: {
-  programs.rofi = {
-    #TODO
-    enable = true;
-    font = "DankMono Nerd Font Mono 10";
-    theme = ./rofi-pine.rasi;
-    package = pkgs.rofi-wayland;
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; {
+  options.gui.launcher.rofi = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        If enabled, rofi will be installed.
+      '';
+    };
   };
-  home.packages = with pkgs; [
-    rofi-rbw # for password management
-    # networkmanager_dmenu # needs dmenu to work
-  ];
+
+  config = mkIf config.gui.launcher.rofi.enable {
+    programs.rofi = {
+      #TODO
+      enable = true;
+      font = "DankMono Nerd Font Mono 10";
+      theme = ./rofi-pine.rasi;
+      package = pkgs.rofi-wayland;
+    };
+    home.packages = with pkgs; [
+      rofi-rbw # for password management
+      # networkmanager_dmenu # needs dmenu to work
+    ];
+  };
 }
