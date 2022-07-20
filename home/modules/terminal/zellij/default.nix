0891c30a1fc8e43f7eeba7149fdfa8eacd54b45e
amirography <amirhossein.alesheikh@gmail.com>
2022-07-20 23:40:53 +0430

    auto: syncing

--- home/modules/terminal/zellij/default.nix
+++ home/modules/terminal/zellij/default.nix
@@ -13,6 +13,13 @@ with lib; {
   config = mkIf config.zellij.enable {
     programs.zellij = {
       enable = true;
+      settings = {
+        default_shell = "fish";
+        simplified_ui = true;
+        pane_frames = false;
+        scroll_buffer_size = 20000;
+        scrollback_editor = "${pkgs.neovim}/bin/nvim";
+      };
     };
   };
 }
