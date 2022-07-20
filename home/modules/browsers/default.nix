249abb02485eb9cd00eb9db5b1b63e115621d3a0
amirography <amirhossein.alesheikh@gmail.com>
2022-07-20 13:07:33 +0430

    auto: syncing

--- home/modules/browsers/default.nix
+++ home/modules/browsers/default.nix
@@ -1,13 +1,29 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+with lib; {
   imports = [
     ./firefox
     ./qutebrowser
     # ./chromium
   ];
-  home.file = {
-    ".config/startpage/" = {
-      source = ./startpage;
-      recursive = true;
+  options = {
+    gui.browser.enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+  config = {
+    gui.browser.firefox.enable = mkIf config.gui.browser.enable true;
+    gui.browser.qutebrowser.enable = mkIf config.gui.browser.enable true;
+    home.file = mkIf config.gui.browser.enable {
+      ".config/startpage/" = {
+        source = ./startpage;
+        recursive = true;
+      };
     };
   };
 }
--- home/modules/browsers/firefox/default.nix
+++ home/modules/browsers/firefox/default.nix
@@ -1,12 +1,24 @@
-{pkgs, ...}: {
-  programs.firefox = {
-    # TODO customize it!
-    enable = true;
-    package = pkgs.firefox-wayland.override {
-      # See nixpkgs' firefox/wrapper.nix to check which options you can use
-      cfg = {
-        # Tridactyl native connector
-        enableTridactylNative = true;
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+with lib; {
+  options = {
+    gui.browser.firefox.enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+  };
+  config = {
+    programs.firefox = mkIf config.gui.browser.firefox.enable {
+      # TODO customize it!
+      enable = true;
+      package = mkIf gui.wayland.enable pkgs.firefox-wayland.override {
+        cfg = {
+          enableTridactylNative = true;
+        };
       };
     };
   };
