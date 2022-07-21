523307a21d1203031d1466879e364a9916f53eeb
amirography <amirhossein.alesheikh@gmail.com>
2022-07-21 14:29:40 +0430

    auto: boot-update

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -3,8 +3,21 @@
   pkgs,
   ...
 }: {
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 120;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.systemd-boot.configurationLimit = 120;
   # boot.loader.generationsDir.enable = true;
+
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+    };
+    grub = {
+      enable = true;
+      device = "nodev";
+      splashMode = "normal";
+      efiSupport = true;
+      backgroundColor = "#191724";
+      # efiInstallAsRemovable = true;
+    };
+  };
 }
