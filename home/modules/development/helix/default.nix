3ae948261925c6936bc1912537351082fff77b86
amirography <amirhossein.alesheikh@gmail.com>
2022-07-23 00:24:12 +0430

    auto: syncing

--- home/modules/development/helix/default.nix
+++ home/modules/development/helix/default.nix
@@ -69,13 +69,13 @@ with lib;
               fg = config.colors.love.hex;
               modifiers = [ "reversed" ];
             };
-            "ui.text" = { fg = config.colors.text.hex; };
+            "ui.text" = { fg = config.colors.subtle.hex; };
             "ui.text.focus" = {
               fg = config.colors.foam.hex;
               modifiers = [ "bold" ];
             };
             "ui.text.info" = {
-              fg = config.colors.text.hex;
+              fg = config.colors.subtle.hex;
               modifiers = [ "bold" ];
             };
             "ui.virtual.whitespace" = config.colors.highlight-med.hex;
