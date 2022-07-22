9ef0c37c689fb481519354e65fb14b0f21ccbe23
amirography <amirhossein.alesheikh@gmail.com>
2022-07-22 07:44:17 +0430

    auto: syncing

--- home/modules/development/default.nix
+++ home/modules/development/default.nix
@@ -13,7 +13,7 @@ with lib; {
     ./git
     ./json
     ./neovim
-	./helix
+    ./helix
     ./nixdev
     ./rust
     ./zig
--- home/modules/development/helix/default.nix
+++ home/modules/development/helix/default.nix
@@ -14,6 +14,30 @@ with lib; {
   config = mkIf config.helix.enable {
     programs.helix = {
       enable = true;
+      languages = [
+        {
+          name = "nix";
+          auto-format = true;
+        }
+      ];
+      settings = {
+        theme = "rose_pine";
+        editor = {
+          line-number = "relative";
+          cursor-shape = {
+            insert = "bar";
+            normal = "block";
+            select = "underline";
+          };
+        };
+      lsp = {
+        display-messages = true;
+      };
+      keys = {
+        normal = {
+        };
+      };
+      };
     };
   };
 }
