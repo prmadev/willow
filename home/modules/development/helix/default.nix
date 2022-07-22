fabf6198d9b055640247c96478e18224ae436f50
amirography <amirhossein.alesheikh@gmail.com>
2022-07-23 00:17:17 +0430

    auto: syncing

--- home/modules/development/helix/default.nix
+++ home/modules/development/helix/default.nix
@@ -75,7 +75,7 @@ with lib;
               modifiers = [ "bold" ];
             };
             "ui.text.info" = {
-              fg = config.colors.text.hex;
+              fg = config.colors.subtle.hex;
               modifiers = [ "bold" ];
             };
             "ui.virtual.whitespace" = config.colors.highlight-med.hex;
