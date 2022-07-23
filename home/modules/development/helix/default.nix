7e14b8d3df0edc85c493c4a9314d594d08e00c14
amirography <amirhossein.alesheikh@gmail.com>
2022-07-23 16:56:42 +0430

    auto: syncing

--- home/modules/development/helix/default.nix
+++ home/modules/development/helix/default.nix
@@ -1,8 +1,7 @@
-{
-  pkgs,
-  lib,
-  config,
-  ...
+{ pkgs
+, lib
+, config
+, ...
 }:
 with lib; {
   options = {
@@ -22,29 +21,25 @@ with lib; {
         }
         {
           name = "html";
-          file-types = ["html" "tmpl"];
+          file-types = [ "html" "tmpl" ];
         }
       ];
 
       themes = {
         rp = {
-          "ui.background" = {bg = config.colors.base.hex;};
+          "ui.background" = { bg = config.colors.base.hex; };
           "ui.menu" = {
             fg = config.colors.subtle.hex;
             bg = config.colors.surface.hex;
-            modifiers = [];
+            modifiers = [ ];
           };
           "ui.menu.selected" = {
             fg = config.colors.text.hex;
             bg = config.colors.pine.hex;
           };
-          "ui.linenr" = {fg = config.colors.overlay.hex;};
+          "ui.linenr" = { fg = config.colors.overlay.hex; };
           "ui.liner.selected" = config.colors.highlight-low.hex;
-          "ui.selection" = {bg = config.colors.highlight-med.hex;};
-          "comment" = {
-            fg = config.colors.muted.hex;
-            modifiers = ["italic" "dim"];
-          };
+          "ui.selection" = { bg = config.colors.highlight-med.hex; };
           "ui.statusline" = {
             fg = config.colors.text.hex;
             bg = config.colors.surface.hex;
@@ -52,17 +47,17 @@ with lib; {
           "ui.statusline.insert" = {
             fg = config.colors.base.hex;
             bg = config.colors.foam.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "ui.statusline.normal" = {
             fg = config.colors.base.hex;
             bg = config.colors.rose.hex;
-            modifiers = [];
+            modifiers = [ ];
           };
           "ui.statusline.select" = {
             fg = config.colors.base.hex;
             bg = config.colors.iris.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "ui.statusline.inactive" = {
             fg = config.colors.iris.hex;
@@ -70,103 +65,121 @@ with lib; {
           };
           "ui.cursor" = {
             fg = config.colors.love.hex;
-            modifiers = ["reversed"];
+            modifiers = [ "reversed" ];
           };
-          "ui.text" = {fg = config.colors.subtle.hex;};
+          "ui.text" = { fg = config.colors.subtle.hex; };
           "ui.text.focus" = {
             fg = config.colors.foam.hex;
-            modifiers = ["bold"];
+            modifiers = [ ];
           };
           "ui.text.info" = {
             fg = config.colors.subtle.hex;
-            modifiers = ["bold"];
+            modifiers = [ ];
           };
-          "ui.virtual.whitespace" = config.colors.love.hex;
-          "operator" = config.colors.gold.hex;
-          "variable" = config.colors.text.hex;
-          "constant.numeric" = config.colors.iris.hex;
-          "constant" = config.colors.gold.hex;
-          "attributes" = config.colors.gold.hex;
-          "type" = config.colors.foam.hex;
+
           "ui.cursor.match" = {
             bg = config.colors.gold.hex;
-            modifiers = ["reversed"];
+            modifiers = [ "reversed" ];
           };
+          "ui.virtual.whitespace" = config.colors.love.hex;
+          "ui.popup" = { bg = config.colors.surface.hex; };
+          "ui.popup.info" = { bg = config.colors.surface.hex; };
+          "ui.window" = { bg = config.colors.base.hex; };
+          "ui.help" = {
+            bg = config.colors.surface.hex;
+            fg = config.colors.foam.hex;
+          };
+
+          "comment" = {
+            fg = config.colors.muted.hex;
+            modifiers = [ "italic" "dim" ];
+          };
+          "operator" = { fg = config.colors.gold.hex; modifiers = [ "bold" ]; };
+          "punctuation" = { fg = config.colors.gold.hex; modifiers = [ "bold" ]; };
+          "variable" = config.colors.text.hex;
+          "constant" = config.colors.gold.hex;
+          "constant.numeric" = config.colors.iris.hex;
+          "constant.character.escape" = config.colors.subtle.hex;
+          "attributes" = config.colors.gold.hex;
+          "type" = config.colors.foam.hex;
           "string" = {
             fg = config.colors.foam.hex;
-            modifiers = ["italic"];
+            modifiers = [ "italic" ];
           };
           "property" = config.colors.foam.hex;
-          "constant.character.escape" = config.colors.subtle.hex;
+
           "function" = config.colors.rose.hex;
           "function.builtin" = config.colors.rose.hex;
           "function.method" = config.colors.rose.hex;
           "constructor" = config.colors.gold.hex;
           "special" = config.colors.gold.hex;
-          "keyword" = {
+
+          "keyword.function" = {
             fg = config.colors.love.hex;
-            modifiers = ["italic"];
+            modifiers = [ "italic" ];
+          };
+          "keyword.control.control" = {
+            fg = config.colors.gold.hex;
+            modifiers = [ ];
           };
           "label" = config.colors.iris.hex;
+
           "namespace" = config.colors.pine.hex;
-          "ui.popup" = {bg = config.colors.surface.hex;};
-          "ui.popup.info" = {bg = config.colors.surface.hex;};
-          "ui.window" = {bg = config.colors.base.hex;};
-          "ui.help" = {
-            bg = config.colors.surface.hex;
-            fg = config.colors.foam.hex;
-          };
+          "tag" = config.colors.foam.hex;
           "text" = config.colors.text.hex;
+
           "diff.plus" = config.colors.foam.hex;
           "diff.delta" = config.colors.rose.hex;
           "diff.minus" = config.colors.love.hex;
+
           "info" = config.colors.gold.hex;
           "hint" = config.colors.gold.hex;
           "debug" = config.colors.rose.hex;
           "diagnostic" = config.colors.rose.hex;
-          "error" = config.colors.love.hex;
+          "error" = { fg = config.colors.love.hex; bg = config.colors.surface.hex; };
+
           "markup.heading.marker" = config.colors.subtle.hex;
           "markup.heading.1" = {
             fg =
               config.colors.love.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "markup.heading.2" = {
             fg = config.colors.gold.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "markup.heading.3" = {
             fg = config.colors.rose.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "markup.heading.4" = {
             fg = config.colors.pine.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "markup.heading.5" = {
             fg = config.colors.foam.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "markup.heading.6" = {
             fg = config.colors.iris.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
-          "markup.list" = {fg = config.colors.love.hex;};
+          "markup.list" = { fg = config.colors.love.hex; };
           "markup.bold" = {
             fg = config.colors.gold.hex;
-            modifiers = ["bold"];
+            modifiers = [ "bold" ];
           };
           "markup.italic" = {
             fg = config.colors.iris.hex;
-            modifiers = ["italic"];
+            modifiers = [ "italic" ];
           };
           "markup.link.url" = {
             fg = config.colors.pine.hex;
-            modifiers = ["underlined"];
+            modifiers = [ "underlined" ];
           };
-          "markup.link.text" = {fg = config.colors.foam.hex;};
-          "markup.quote" = {fg = config.colors.rose.hex;};
-          "markup.raw" = {fg = config.colors.foam.hex;};
+          "markup.link.text" = { fg = config.colors.foam.hex; };
+          "markup.quote" = { fg = config.colors.rose.hex; };
+          "markup.raw" = { fg = config.colors.foam.hex; };
         };
       };
       settings = {
