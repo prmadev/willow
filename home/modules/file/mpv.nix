d5f004558040ae1ec71bba6a7f491c12c71ff971
amirography <amirhossein.alesheikh@gmail.com>
2022-07-27 18:27:44 +0430

    auto: syncing

--- home/modules/file/mpv.nix
+++ home/modules/file/mpv.nix
@@ -17,11 +17,11 @@ with lib; {
       package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override {vapoursynthSupport = true;}) {youtubeSupport = true;};
       config = {
         ytdl-format = "bestvideo+bestaudio";
-        background = config.colors.base.hex;
-        osd-color = config.colors.base.hex;
-        sub-color = config.colors.base.hex;
-        osd-border-color = config.colors.text.hex;
-        sub-border-color = config.colors.text.hex;
+        background = config.cat.mocha.base.hex;
+        osd-color = config.cat.mocha.base.hex;
+        sub-color = config.cat.mocha.base.hex;
+        osd-border-color = config.cat.mocha.text.hex;
+        sub-border-color = config.cat.mocha.text.hex;
         sub-font = "monospace";
         sub-scale = "0.5";
       };
