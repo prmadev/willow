0c90ed982c08156a80512b3e9a0cbb2d57f16554
amirography <amirhossein.alesheikh@gmail.com>
2022-07-28 08:10:46 +0430

    auto: syncing

--- home/modules/launcher/rofi/default.nix
+++ home/modules/launcher/rofi/default.nix
@@ -1,8 +1,7 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
+{ pkgs
+, config
+, lib
+, ...
 }:
 with lib; {
   options.launcher.rofi = {
@@ -20,7 +19,88 @@ with lib; {
       #TODO
       enable = true;
       font = config.global-fonts.main-family + " 10";
-      theme = ./rofi-pine.rasi;
+      theme = let inherit (config.lib.formats.rasi) mkLiteral; in
+        {
+
+          "*" = {
+            bg = mkLiteral config.cat.mocha.surface0.hex;
+            fg = mkLiteral config.cat.mocha.text.hex;
+            button = mkLiteral config.cat.mocha.green.hex;
+            background-color = mkLiteral config.cat.mocha.surface0.hex;
+            text-color = mkLiteral config.cat.mocha.text.hex;
+          };
+
+          window = {
+            transparency = mkLiteral "real";
+            border-radius = mkLiteral "0px";
+            width = mkLiteral "20%";
+            y-offset = mkLiteral "00px";
+            padding = mkLiteral "0px 00px 0px 0px";
+            location = 0;
+          };
+
+          prompt = {
+            enabled = false;
+          };
+
+          entry = {
+            placeholder = "";
+            expand = true;
+            padding = mkLiteral "5px 5px 5px 5px";
+            background-color = mkLiteral config.cat.mocha.surface0.hex;
+            placeholder-color = mkLiteral config.cat.mocha.surface0.hex;
+            text-color = mkLiteral config.cat.mocha.text.hex;
+            font = "monospace black 10";
+          };
+
+          inputbar = {
+            children = map mkLiteral [ "prompt" "entry" ];
+            background-color = mkLiteral config.cat.mocha.surface0.hex;
+            text-color = mkLiteral config.cat.mocha.text.hex;
+            expand = false;
+            border-radius = mkLiteral "0px";
+            padding = mkLiteral "5px";
+            font = "monospace black 10";
+          };
+
+          listview = {
+            columns = 1;
+            lines = 12;
+            cycle = true;
+            dynamic = false;
+            layout = mkLiteral "vertical";
+          };
+
+          mainbox = {
+            children = map mkLiteral [ "inputbar" "listview" ];
+            spacing = mkLiteral "0px";
+            padding = mkLiteral "0px 0px 0px 0px";
+          };
+
+          element = {
+            orientation = mkLiteral "vertical";
+            border-radius = mkLiteral "0px";
+            padding = mkLiteral "5px 5px 5px 5px";
+            text-color = mkLiteral config.cat.mocha.subtext0.hex;
+          };
+
+          element-text = {
+            expand = true;
+            vertical-align = mkLiteral "0.5";
+            margin = mkLiteral "5px 5px 5px 5px";
+            background-color = mkLiteral "inherit";
+            text-color = mkLiteral "inherit";
+            font = "monospace 10";
+          };
+
+
+          "element selected" = {
+            background-color = mkLiteral config.cat.mocha.green.hex;
+            text-color = mkLiteral config.cat.mocha.surface0.hex;
+            border-radius = mkLiteral "0px";
+            font = "monospace Black 10";
+          };
+        };
       package = pkgs.rofi-wayland;
     };
     home.packages = with pkgs; [
