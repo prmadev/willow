aac35612c23a9a300bf4cc8dcdadbe1af308a18d
amirography <amirhossein.alesheikh@gmail.com>
2022-08-01 09:33:47 +0430

    auto: syncing

--- system/docker/default.nix
+++ system/docker/default.nix
@@ -3,6 +3,11 @@
   pkgs,
   ...
 }: {
-  virtualisation.docker.enable = true;
-  virtualisation.docker.rootless.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+    };
+  };
 }
