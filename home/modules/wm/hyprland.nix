bc8587f5c5918edcffefdc3bf9bafdb42462e051
amirography <amirhossein.alesheikh@gmail.com>
2022-08-02 23:45:09 +0430

    auto: syncing

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -67,7 +67,24 @@
 
         bind=SUPER,1,workspace,1
         bind=SUPER,2,workspace,2
+        bind=SUPER,3,workspace,3
+        bind=SUPER,4,workspace,4
+        bind=SUPER,5,workspace,5
+        bind=SUPER,6,workspace,6
+        bind=SUPER,7,workspace,7
+        bind=SUPER,8,workspace,8
+        bind=SUPER,9,workspace,9
 
+
+        bind=SUPERSHIFT,1,movetoworkspace,1
+        bind=SUPERSHIFT,2,movetoworkspace,2
+        bind=SUPERSHIFT,3,movetoworkspace,3
+        bind=SUPERSHIFT,4,movetoworkspace,4
+        bind=SUPERSHIFT,5,movetoworkspace,5
+        bind=SUPERSHIFT,6,movetoworkspace,6
+        bind=SUPERSHIFT,7,movetoworkspace,7
+        bind=SUPERSHIFT,8,movetoworkspace,8
+        bind=SUPERSHIFT,9,movetoworkspace,9
 
       }
     '';
