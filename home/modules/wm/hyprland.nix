ea06126c0c08c2919c88de555cd639332ce6136d
amirography <amirhossein.alesheikh@gmail.com>
2022-08-02 22:57:16 +0430

    auto: syncing

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -31,8 +31,8 @@
 
       input {
         follow_mouse=1
-        repeat_rate=100
-        repeat_delay=100
+        repeat_rate=50
+        repeat_delay=150
       }
       misc {
         disable_hyprland_logo=true
