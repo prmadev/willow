b90928a89ed895cc9cdd8780615d8eb8b8832a60
amirography <amirhossein.alesheikh@gmail.com>
2022-08-03 10:22:22 +0430

    auto: syncing

--- home/modules/wm/eww/default.nix
+++ home/modules/wm/eww/default.nix
@@ -15,5 +15,34 @@ with lib; {
     home.packages = with pkgs; [
       eww-wayland
     ];
+
+    # configuration
+    home.file.".config/eww/eww.scss".source = ./eww.scss;
+    home.file.".config/eww/eww.yuck".source = ./eww.yuck;
+
+    home.file.".config/eww/scripts/workspaces.sh" = {
+      source = ./scripts/workspaces.sh;
+      executable = true;
+    };
+
+    home.file.".config/eww/scripts/workspaces.lua" = {
+      source = ./scripts/workspaces.lua;
+      executable = true;
+    };
+
+    home.file.".config/eww/scripts/battery.sh" = {
+      source = ./scripts/battery.sh;
+      executable = true;
+    };
+
+    home.file.".config/eww/scripts/wifi.sh" = {
+      source = ./scripts/wifi.sh;
+      executable = true;
+    };
+
+    home.file.".config/eww/scripts/brightness.sh" = {
+      source = ./scripts/brightness.sh;
+      executable = true;
+    };
   };
 }
