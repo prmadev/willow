479e872afeae74c01b543c2c7a31ea23c54e9952
amirography <amirhossein.alesheikh@gmail.com>
2022-08-03 09:32:18 +0430

    auto: syncing

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -34,7 +34,7 @@
 
       monitors {
         monitor=eDP-1,1366x768@60,0x0,1
-        monitor=DP-1,1600x900@60,0x0,1
+        monitor=DP-1,1600x900@60,1366x0,1
       }
       input {
         follow_mouse=1
