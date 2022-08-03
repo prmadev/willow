ba11987641e92002a0a10c14dbd9eaeecde7ed41
amirography <amirhossein.alesheikh@gmail.com>
2022-08-03 23:33:52 +0430

    auto: syncing

--- home/modules/wm/waybar/default.nix
+++ home/modules/wm/waybar/default.nix
@@ -16,6 +16,7 @@ with lib; {
     home.packages = with pkgs; [
       ninja
       meson
+      pkgconfig
       llvmPackages.bintools-unwrapped
     ];
     programs.waybar = mkIf config.waybar.enable {
