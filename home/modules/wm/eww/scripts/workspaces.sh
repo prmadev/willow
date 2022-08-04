41b00c2469dea25b4d857f0602c128651a3fb816
amirography <amirhossein.alesheikh@gmail.com>
2022-08-04 10:05:23 +0430

    auto: syncing

--- home/modules/wm/eww/scripts/workspaces.sh
+++ home/modules/wm/eww/scripts/workspaces.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 workspaces() {
-./scripts/workspaces.lua
+./workspaces.lua
 }
 workspaces
 tail -f /tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/hyprland.log | grep --line-buffered "Changed to workspace" | while read -r; do 
