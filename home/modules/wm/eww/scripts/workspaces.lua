005f1ee3169e8bca8d6b14b1968edd906a09e424
amirography <amirhossein.alesheikh@gmail.com>
2022-08-04 10:00:57 +0430

    auto: syncing

--- home/modules/wm/eww/scripts/workspaces.lua
+++ home/modules/wm/eww/scripts/workspaces.lua
@@ -17,10 +17,10 @@ box = "(box :orientation \"v\" :spacing 1 :space-evenly \"true\" "
 for i = 1, #existing_workspaces do
     local c = existing_workspaces:sub(i,i)
     if tonumber(c) == tonumber(active_workspace) then
-        local btn = "(button :class \"active\" :onclick \"hyprctl dispatch workspace "..c.." \" \"\")"
+        local btn = "(button :class \"active\" )"
         box = box .. btn
     elseif c ~= "\n" then
-        local btn = "(button :class \"inactive\" :onclick \"hyprctl dispatch workspace "..c.."\" \"\")"
+        local btn = "(button :class \"inactive\" )"
         box = box .. btn
     end
 end
