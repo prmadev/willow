f4e4041d270014ddd52417982c26a8e5276d4547
amirography <amirhossein.alesheikh@gmail.com>
2022-08-05 18:59:33 +0430

    auto: syncing

--- home/modules/wm/waybar/default.nix
+++ home/modules/wm/waybar/default.nix
@@ -51,63 +51,53 @@ with lib; {
             "battery"
           ];
           "custom/hyprgomon" = {
-            interval = 1;
             exec = "hyprgo -a";
             return-type = "json";
             format = " {} ";
           };
 
           "custom/hyprgows1" = {
-            interval = 0.1;
             exec = "hyprgo -w 1";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows2" = {
-            interval = 0.1;
             exec = "hyprgo -w 2";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows3" = {
-            interval = 0.1;
             exec = "hyprgo -w 3";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows4" = {
-            interval = 0.1;
             exec = "hyprgo -w 4";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows5" = {
-            interval = 0.1;
             exec = "hyprgo -w 5";
             return-type = "json";
             format = " {} ";
           };
 
           "custom/hyprgows6" = {
-            interval = 0.1;
             exec = "hyprgo -w 6";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows7" = {
-            interval = 0.1;
             exec = "hyprgo -w 7";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows8" = {
-            interval = 0.1;
             exec = "hyprgo -w 8";
             return-type = "json";
             format = " {} ";
           };
           "custom/hyprgows9" = {
-            interval = 0.1;
             exec = "hyprgo -w 9";
             return-type = "json";
             format = " {} ";
