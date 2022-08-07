c9216137351bfba9917947efadcdd70a9a75571f
amirography <amirhossein.alesheikh@gmail.com>
2022-08-07 09:39:28 +0430

    auto: syncing

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -10,7 +10,7 @@
     extraConfig = ''
       exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY HYPRLAND_INSTANCE_SIGNATURE
       exec-once=systemctl --user start hyprland-session.target
-      exec=swaybg -c "#191724"
+      exec=swaybg -c "\#191724"
       exec-once=eww open-all
 
       general {
