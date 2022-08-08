7665db8da909379315a50bf092be4fd0e4d2e41e
amirography <amirhossein.alesheikh@gmail.com>
2022-08-08 10:38:38 +0430

    auto: syncing

--- home/modules/network/proxy.nix
+++ home/modules/network/proxy.nix
@@ -14,7 +14,7 @@ with lib; {
   config = mkIf config.proxy.enable {
     home.packages = with pkgs; [
       clash # for networking #TODO
-      bore-cli
+      shadowsocks-rust
     ];
 
     home.file = {
