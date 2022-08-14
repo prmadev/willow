5fcf5c401826865a487566e0f27b63dea4171601
amirography <amirhossein.alesheikh@gmail.com>
2022-08-14 11:18:38 +0430

    auto: syncing

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -81,8 +81,8 @@ with lib; {
             bind=SUPER,q,killactive
 
 
-            bind=SUPER,f,fullscreen,0
-            bind=SUPER,e,fullscreen,1
+            bind=SUPER,e,fullscreen,0
+            bind=SUPER,f,fullscreen,1
 
             bind=SUPER,h,movefocus,l
             bind=SUPER,j,movefocus,d
@@ -101,15 +101,15 @@ with lib; {
             bind=SUPERALT,l,resizeactive,10 0
 
 
-            bind=SUPER,1,workspace,m+1
-            bind=SUPER,2,workspace,m+2
-            bind=SUPER,3,workspace,m+3
-            bind=SUPER,4,workspace,m+4
-            bind=SUPER,5,workspace,m+5
-            bind=SUPER,6,workspace,m+6
-            bind=SUPER,7,workspace,m+7
-            bind=SUPER,8,workspace,m+8
-            bind=SUPER,9,workspace,m+9
+            bind=SUPER,1,workspace,1
+            bind=SUPER,2,workspace,2
+            bind=SUPER,3,workspace,3
+            bind=SUPER,4,workspace,4
+            bind=SUPER,5,workspace,5
+            bind=SUPER,6,workspace,6
+            bind=SUPER,7,workspace,7
+            bind=SUPER,8,workspace,8
+            bind=SUPER,9,workspace,9
 
 
             bind=SUPERSHIFT,exclam,movetoworkspace,1
