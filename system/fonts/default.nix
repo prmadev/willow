5bcf2fb8f2b37c7c232db3487907023efa01290f
amirography <amirhossein.alesheikh@gmail.com>
2022-08-22 09:58:27 +0430

    auto: syncing

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -12,14 +12,6 @@
   fonts = {
     # enableDefaultFonts = true; # just to see if it helps with faulty characters.
     fontconfig = {
-      localConf = ''
-        <alias>
-            <family>monospace</family>
-            <prefer>
-                <family>${config.global-fonts.main-family}</family>
-            </prefer>
-        </alias>
-      '';
       defaultFonts = {
         serif = config.global-fonts.main-set;
         sansSerif = config.global-fonts.main-set;
