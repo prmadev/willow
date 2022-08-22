5166a2fe45927b61fb4364d73ad24f330249affc
amirography <amirhossein.alesheikh@gmail.com>
2022-08-22 09:49:22 +0430

    auto: syncing

--- system/fonts/default.nix
+++ system/fonts/default.nix
@@ -10,8 +10,16 @@
     (nerdfonts.override {fonts = ["JetBrainsMono" "CascadiaCode" "DaddyTimeMono"];})
   ];
   fonts = {
-    enableDefaultFonts = true; # just to see if it helps with faulty characters.
+    # enableDefaultFonts = true; # just to see if it helps with faulty characters.
     fontconfig = {
+      localConf = ''
+        <alias>
+            <family>monospace</family>
+            <prefer>
+                <family>${config.global-fonts.main-family}</family>
+            </prefer>
+        </alias>
+      '';
       defaultFonts = {
         serif = config.global-fonts.main-set;
         sansSerif = config.global-fonts.main-set;
