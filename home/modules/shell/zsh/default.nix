1027c98bab59dd7fa458578080e034e849af67ae
amirography <amirhossein.alesheikh@gmail.com>
2022-08-29 08:19:47 +0430

    auto: syncing

--- home/modules/shell/zsh/default.nix
+++ home/modules/shell/zsh/default.nix
@@ -27,7 +27,7 @@ with lib; {
 
       autocd = true;
       initExtra = ''
-        echo 'eval "$(zellij setup --generate-auto-start zsh)"' >> ~/.zshrc
+        'eval "$(zellij setup --generate-auto-start zsh)"'
         if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ ! "$TERM" =~ screen ]] && [[ ! "$TERM" =~ tmux ]] && [ -z "$TMUX" ]; then
         			exec tmux
         fi
