0e0c3b45b8175d412d84a5d982b4803245dd3364
amirography <amirhossein.alesheikh@gmail.com>
2022-08-30 11:11:12 +0430

    auto: syncing

--- home/modules/development/helix/default.nix
+++ home/modules/development/helix/default.nix
@@ -46,61 +46,43 @@ with lib; {
           };
         }
       ];
-
-      # themes = let
-      #   catppuccin_helix = inputs.helix-catppuccin;
-      #   # catppuccin_helix = pkgs.fetchFromGitHub {
-      #   #   owner = "catppuccin";
-      #   #   repo = "helix";
-      #   #   rev = "dc1d236f610fa9573fa59194c79dd3a5a9c8a639";
-      #   #   sha256 = "sha256-JfTS1Kgcdd/Gu05QXWwztHlr9zrIy73YXLvx7iaYAqM=";
-      #   # };
-
-      #   variants = ["catppuccin_latte" "catppuccin_frappe" "catppuccin_macchiato" "catppuccin_mocha"];
-      # in
-      #   lib.genAttrs variants (n: builtins.fromTOML (builtins.readFile "${catppuccin_helix}/italics/${n}.toml"));
-
       settings = {
-        # theme = "catppuccin-mocha";
         theme = "rp";
-
-        # indent-guides.render = true;
         editor = {
           auto-format = true;
           completion-trigger-len = 1;
           line-number = "relative";
           color-modes = true;
+          cursorline = true;
+          idle-timeout = 0;
+          auto-pairs = true;
+
+          lsp.display-messages = true;
+          file-picker.hidden = false;
+          indent-guides = {
+            render = true;
+            character = "│";
+          };
+
           cursor-shape = {
             insert = "bar";
             normal = "block";
             select = "block";
           };
+
+          statusline = {
+            left = ["file-name"];
+            center = ["mode"];
+            right = ["selections" "diagnostics" "spinner" "position-percentage"];
+          };
         };
 
         keys = {
           normal = {
-            ret = ["open_below" "normal_mode"];
+            # ret = ["open_below" "normal_mode"];
             space = {
-              c = {
-                f = ":format";
-                w = ":w!";
-                q = ":q";
-                b = ":bc";
-              };
-              g = {
-                i = ["select_all" ":pipe goimport"];
-                f = ["select_all" ":pipe gofumports"];
-
-                t = ":run-shell-command go mod tidy";
-                g = ":run-shell-command go get -u";
-              };
-              n = {
-                f = ["select_all" ":pipe alejandra"];
-              };
+              c = ":bc";
             };
-          };
-          insert = {
-            "A-ret" = "open_below";
           };
         };
       };
