286c1af55a753163fe7571a0c9838b4c42442efd
amirography <amirhossein.alesheikh@gmail.com>
2022-10-25 10:52:49 +0330

    auto: syncing

--- home/modules/terminal/wezterm/wezterm.lua
+++ home/modules/terminal/wezterm/wezterm.lua
@@ -1,7 +1,8 @@
+local wezterm = require 'wezterm'
 return {
 	color_scheme = "Rosé Pine (base16)",
 	hide_tab_bar_if_only_one_tab = true,
-	default_prog = { "${pkgs.zsh}/bin/zsh", },
+	default_prog = { "zsh", },
 	font = wezterm.font 'monospace',
 
 }
