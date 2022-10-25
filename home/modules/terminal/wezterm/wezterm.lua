4dd8c56f145b00531f54b09b6aa3c42c97787173
amirography <amirhossein.alesheikh@gmail.com>
2022-10-25 13:18:03 +0330

    auto: syncing

--- home/modules/terminal/wezterm/wezterm.lua
+++ home/modules/terminal/wezterm/wezterm.lua
@@ -33,4 +33,16 @@ return {
 		WSLENV = 'TERMINFO_DIRS',
 	},
 	term = "wezterm",
+	window_frame = {
+		inactive_titlebar_bg = '#353535',
+		active_titlebar_bg = '#2b2042',
+		inactive_titlebar_fg = '#cccccc',
+		active_titlebar_fg = '#ffffff',
+		inactive_titlebar_border_bottom = '#2b2042',
+		active_titlebar_border_bottom = '#2b2042',
+		button_fg = '#cccccc',
+		button_bg = '#2b2042',
+		button_hover_fg = '#ffffff',
+		button_hover_bg = '#3b3052',
+	},
 }
