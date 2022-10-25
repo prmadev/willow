6a4168bb43f5d7ada5afb5d85416744d90bcb8a7
amirography <amirhossein.alesheikh@gmail.com>
2022-10-25 10:50:34 +0330

    auto: syncing

--- home/modules/terminal/wezterm/default.nix
+++ home/modules/terminal/wezterm/default.nix
@@ -14,14 +14,7 @@ with lib; {
   config = mkIf config.wezterm.enable {
     programs.wezterm = {
       enable = true;
-      extraConfig = ''
-                    return {
-                     color_scheme = "Rosé Pine (base16)",
-                 hide_tab_bar_if_only_one_tab = true,
-                 default_prog =  {"${pkgs.zsh}/bin/zsh",},
-        font = wezterm.font 'monospace',
-              }
-      '';
+      extraConfig = builtins.readFile ./wezterm.lua;
     };
   };
 }
---
+++ home/modules/terminal/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+return {
+	color_scheme = "Rosé Pine (base16)",
+	hide_tab_bar_if_only_one_tab = true,
+	default_prog = { "${pkgs.zsh}/bin/zsh", },
+	font = wezterm.font 'monospace',
+
+}
