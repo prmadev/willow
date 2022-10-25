2d8315394e53d786863431110c5a0371bf8769e9
amirography <amirhossein.alesheikh@gmail.com>
2022-10-25 10:54:05 +0330

    auto: syncing

--- home/modules/terminal/wezterm/wezterm.lua
+++ home/modules/terminal/wezterm/wezterm.lua
@@ -3,6 +3,7 @@ return {
 	color_scheme = "Rosé Pine (base16)",
 	hide_tab_bar_if_only_one_tab = true,
 	default_prog = { "zsh", },
-	font = wezterm.font 'monospace',
+	font = wezterm.font('monospace', { weight = 'Bold', italic = true }),
+
 
 }
