3d538b8bb8e634768398c2270d0551c55eed828e
amirography <amirhossein.alesheikh@gmail.com>
2022-10-25 11:54:32 +0330

    auto: syncing

--- home/modules/terminal/wezterm/wezterm.lua
+++ home/modules/terminal/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require 'wezterm'
 return {
 	color_scheme = "Rosé Pine (base16)",
 	hide_tab_bar_if_only_one_tab = true,
-	default_prog = { "zsh", },
+	default_prog = { "zsh" },
 
 	font = wezterm.font_with_fallback {
 		{
@@ -28,44 +28,42 @@ return {
 
 			},
 		},
-	},
 
-	-- Similarly, a fancy bold+italic font
-	{
-		italic = true,
-		intensity = 'Bold',
-		font = wezterm.font_with_fallback {
-			{
-				family = 'Operator Mono SSm Nerd Lig',
-				weight = 325,
-				style = "Italic",
+		-- Similarly, a fancy bold+italic font
+		{
+			italic = true,
+			intensity = 'Bold',
+			font = wezterm.font_with_fallback {
+				{
+					family = 'Operator Mono SSm Nerd Lig',
+					weight = 325,
+					style = "Italic",
+				},
 			},
 		},
-	},
 
-	-- Make regular bold text a different color to make it stand out even more
-	{
-		intensity = 'Bold',
-		font = wezterm.font_with_fallback {
-			{
-				family = 'Operator Mono SSm Nerd Lig',
-				weight = 325,
-				style = "Normal",
+		-- Make regular bold text a different color to make it stand out even more
+		{
+			intensity = 'Bold',
+			font = wezterm.font_with_fallback {
+				{
+					family = 'Operator Mono SSm Nerd Lig',
+					weight = 325,
+					style = "Normal",
+				},
 			},
 		},
-	},
 
-	-- For half-intensity text, use a lighter weight font
-	{
-		intensity = 'Half',
-		font = wezterm.font_with_fallback {
-			{
-				family = 'Operator Mono SSm Nerd Lig',
-				weight = 325,
-				style = "Normal",
+		-- For half-intensity text, use a lighter weight font
+		{
+			intensity = 'Half',
+			font = wezterm.font_with_fallback {
+				{
+					family = 'Operator Mono SSm Nerd Lig',
+					weight = 325,
+					style = "Normal",
+				},
 			},
 		},
-	},
-
-
+	}
 }
