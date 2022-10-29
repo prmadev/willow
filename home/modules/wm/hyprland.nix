14fa6fa548f6349bda4be75d43c4112c506ce40c
amirography <amirhossein.alesheikh@gmail.com>
2022-10-29 14:03:36 +0330

    auto: syncing

--- home/modules/wm/hyprland.nix
+++ home/modules/wm/hyprland.nix
@@ -15,11 +15,9 @@ with lib; {
     wayland.windowManager.hyprland = {
       enable = true;
       extraConfig = ''
-           exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY HYPRLAND_INSTANCE_SIGNATURE
-           exec-once=systemctl --user start hyprland-session.target
-        exec=pkill wh
-        exec=wh
-           exec-once=hyprpaper
+           # exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY HYPRLAND_INSTANCE_SIGNATURE
+           # exec-once=systemctl --user start hyprland-session.target
+           # exec-once=hyprpaper
            # exec-once=eww open-many topbar topbar-monitor-two info
            # exec=eww reload
 
