2c3eff21cf4ef9b0cd220fc54028b06728b4ce0b
amirography <amirhossein.alesheikh@gmail.com>
2022-11-02 11:59:38 +0330

    auto: syncing

--- system/nix/default.nix
+++ system/nix/default.nix
@@ -17,11 +17,11 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
-        "https://hyprland.cachix.org"
+        # "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        # "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
   };
