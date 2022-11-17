ef8671b3bbd9dda978ffd46cc19f134f50047464
amirography <amirhossein.alesheikh@gmail.com>
2022-11-17 16:15:27 +0330

    feat(): make quick daily changes

--- system/database/default.nix
+++ system/database/default.nix
@@ -18,50 +18,38 @@ with lib; {
       pgweb
       # gobang
     ];
+    services.postgresql = {
+      enable = true;
 
-    # services.pgadmin.enable = true;
-    containers.database = {
-      config = {
-        config,
-        pkgs,
-        ...
-      }: {
-        networking.firewall.allowedTCPPorts = [5432];
-        services.postgresql = {
-          enable = true;
+      enableTCPIP = true;
+      port = 5432;
 
-          enableTCPIP = true;
-          port = 5432;
+      authentication = "host all all 10.233.0.0/16 trust";
+      ensureUsers = [
+        {
+          name = "superuser";
+          ensurePermissions = {
+            "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
+            "DATABASE work" = "ALL PRIVILEGES";
+          };
+        }
+        {
+          name = "a";
+          ensurePermissions = {
+            "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
 
-          authentication = "host all all 10.233.0.0/16 trust";
-          ensureUsers = [
-            {
-              name = "superuser";
-              ensurePermissions = {
-                "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
-                "DATABASE work" = "ALL PRIVILEGES";
-              };
-            }
-            {
-              name = "a";
-              ensurePermissions = {
-                "ALL TABLES IN SCHEMA public" = "ALL PRIVILEGES";
-
-                "DATABASE a" = "ALL PRIVILEGES";
-                "DATABASE work" = "ALL PRIVILEGES";
-              };
-            }
-          ];
-          ensureDatabases = [
-            "work"
-          ];
-        };
-
-        services.pgadmin.initialEmail = "amirhossein.alesheikh@gmail.com";
-
-        services.pgadmin.initialPasswordFile = ./enter;
-      };
+            "DATABASE work" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+      ensureDatabases = [
+        "work"
+      ];
     };
-    # services.pgadmin.
+
+    services.pgadmin.enable = true;
+    services.pgadmin.initialEmail = "amirhossein.alesheikh@gmail.com";
+    services.pgadmin.initialPasswordFile = ./enter;
   };
+  # services.pgadmin.
 }
