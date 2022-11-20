03962c04cf262816cf14e5bf3830025e2b9e2bfa
amirography <amirhossein.alesheikh@gmail.com>
2022-11-20 11:37:01 +0330

    feat(): make quick daily changes

--- home/modules/development/neovim/default.nix
+++ home/modules/development/neovim/default.nix
@@ -15,16 +15,71 @@ with lib; {
   config = mkIf config.neovim.enable {
     # NixVim
     programs.nixvim = {
-      # enabling the whole thing
+      # --- enabling the whole thing --- #
       enable = true;
 
-      # color settings
+      # --- color settings ---#
       colorscheme = "rose-pine";
 
-      # plugins to have
+      # --- plugins to have --- #
       extraPlugins = with pkgs.vimPlugins; [
         rose-pine
       ];
+
+      # --- plugins --- #
+      plugins = {
+        # ---  ui
+        # tabs and buffers
+        bufferline = {
+          enable = true;
+          diagnostics = "nvim_lsp";
+        };
+
+        # bottom status
+        lualine = {
+          enable = true;
+        };
+
+        floaterm = {
+          enable = true;
+          autoInsert = true;
+          shell = "${pkgs.zsh}/bin/zsh";
+        };
+
+        gitgutter = {
+          enable = true;
+        };
+
+        # --- cmp
+        cmp-nvim-lsp.enable = true;
+        cmp-dap.enable = true;
+        cmp-nvim-lsp-document-symbol.enable = true;
+        cmp-nvim-lsp-signature-help.enable = true;
+        cmp-nvim-lua.enable = true;
+        cmp-path.enable = true;
+        cmp-treesitter.enable = true;
+        cmp_luasnip.enable = true;
+
+        # --- editing
+        comment-nvim.enable = true;
+
+        # lsp
+        lsp = {
+          enable = true;
+          servers = {
+            cssls.enable = true;
+            gopls.enable = true;
+            html.enable = true;
+            jsonls.enable = true;
+            rnix-lsp.enable = true;
+            rust-analyzer.enable = true;
+          };
+        };
+
+        # --- language speciifc
+        # rust
+        crates-nvim.enable = true;
+      };
     };
 
     home.packages = with pkgs; [
