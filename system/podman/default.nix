a928a43a9258fd4002f2cd7c72833c66456c7d24
amirography <amirhossein.alesheikh@gmail.com>
2022-12-19 12:00:15 +0330

    feat(): make quick daily changes

--- system/podman/default.nix
+++ system/podman/default.nix
@@ -1,7 +1,12 @@
-{...}: {
+{pkgs, ...}: {
   virtualisation.podman = {
     enable = true;
     dockerSocket.enable = true;
     dockerCompat = true;
+    defaultNetwork.dnsname.enable = true; # for arion
   };
+  environment.systemPackages = [
+    pkgs.arion # for arion
+    pkgs.docker-client # for arion
+  ];
 }
