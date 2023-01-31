5563c118473f1d4b47b1509fe8511fa0d2e67347
amirography <amirhossein.alesheikh@gmail.com>
2023-01-31 16:39:42 +0330

    feat(): make quick daily changes

---
+++ home/modules/shell/nu/config.nu
@@ -0,0 +1,19 @@
+
+        let-env STARSHIP_SHELL = "nu"
+
+
+def create_left_prompt [] {
+    starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
+}
+
+# Use nushell functions to define your right and left prompt
+let-env PROMPT_COMMAND = { create_left_prompt }
+let-env PROMPT_COMMAND_RIGHT = ""
+                      let-env config = {
+                           filesize_metric: false
+                           table_mode: compact
+                           use_ls_colors: false
+                	 EDITOR: nvim
+                	 VISUAL: nvim
+                	}
+                source ~/.cache/starship/init.nu
--- home/modules/shell/nu/default.nix
+++ home/modules/shell/nu/default.nix
@@ -17,14 +17,28 @@ with lib; {
         starship init nu | save ~/.cache/starship/init.nu
       '';
       configFile.text = ''
+                 let-env STARSHIP_SHELL = "nu"
+
+
+                    def create_left_prompt [] {
+                    	starship prompt --cmd-duration $env.CMD_DURATION_MS $'--status=($env.LAST_EXIT_CODE)'
+                    }
+
+                    let-env PROMPT_COMMAND = { create_left_prompt }
+
+                    let-env PROMPT_COMMAND_RIGHT = ""
+        let-env PROMPT_INDICATOR = ""
+        let-env PROMPT_INDICATOR_VI_INSERT = ": "
+        let-env PROMPT_INDICATOR_VI_NORMAL = "〉"
+        let-env PROMPT_MULTILINE_INDICATOR = "::: "
               let-env config = {
-                   filesize_metric: false
-                   table_mode: compact
-                   use_ls_colors: false
-        	 EDITOR: nvim
-        	 VISUAL: nvim
-        	}
-        source ~/.cache/starship/init.nu
+              		 filesize_metric: false
+              		 table_mode: compact
+              		 use_ls_colors: false
+                    		 EDITOR: nvim
+                    		 VISUAL: nvim
+              }
+                    source ~/.cache/starship/init.nu
       '';
     };
   };
