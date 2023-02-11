396ffdb5dcfd84a8c4cf2bdfaaf1c70c9ed6ac72
amirography <amirhossein.alesheikh@gmail.com>
2023-02-11 20:39:03 +0330

    feat(): make quick daily changes

--- home/modules/style/font.nix
+++ home/modules/style/font.nix
@@ -19,9 +19,17 @@ with lib; {
   config = mkIf config.font.enable {
     fonts.fontconfig.enable = true;
     gtk = mkIf config.gtkconf.enable {
+      enable = true;
       font = {
         name = config.font.main;
         size = 12;
+      };
+      theme = {
+        name = "Catppuccin-Macchiato-Standard-Pink-Dark";
+        package = pkgs.catppuccin-gtk.override {
+          accents = ["pink"];
+          variant = "macchiato";
+        };
       };
     };
     home.packages = with pkgs; [
