bd3c86bdc5c2c1f842291376c1ebee60d2c5023c
amirography <amirhossein.alesheikh@gmail.com>
2023-02-11 22:27:28 +0330

    feat(): make quick daily changes

--- home/modules/file/mpv.nix
+++ home/modules/file/mpv.nix
@@ -17,11 +17,11 @@ with lib; {
       package = pkgs.wrapMpv (pkgs.mpv-unwrapped.override {vapoursynthSupport = true;}) {youtubeSupport = true;};
       config = {
         ytdl-format = "bestvideo+bestaudio";
-        background = config.colors.base.hex;
-        osd-color = config.colors.base.hex;
-        sub-color = config.colors.base.hex;
-        osd-border-color = config.colors.text.hex;
-        sub-border-color = config.colors.text.hex;
+        background = config.colors.macchiato.base.hex;
+        osd-color = config.colors.macchiato.base.hex;
+        sub-color = config.colors.macchiato.base.hex;
+        osd-border-color = config.colors.macchiato.text.hex;
+        sub-border-color = config.colors.macchiato.text.hex;
         sub-font = "monospace";
         sub-scale = "0.5";
       };
