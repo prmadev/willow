259dd51644a79eb340a9ff6e0e45754efe66af88
amirography <amirhossein.alesheikh@gmail.com>
2023-03-11 12:08:43 +0330

    feat(): make quick daily changes

--- home/modules/development/helix/catppuccin-improved.nix
+++ home/modules/development/helix/catppuccin-improved.nix
@@ -38,19 +38,19 @@
     };
     "ui.linenr" = {
       fg = config.colors.macchiato.text.hex;
-      bg = config.colors.macchiato.mantle.hex;
+      bg = config.colors.macchiato.surface1.hex;
     };
     #
     "ui.gutter" = {
-      bg = config.colors.macchiato.mantle.hex;
+      bg = config.colors.macchiato.surface1.hex;
     };
     "ui.gutter.selected" = {
-      fg = config.colors.macchiato.mantle.hex;
+      fg = config.colors.macchiato.surface1.hex;
       bg = config.colors.macchiato.sky.hex;
       modifiers = ["bold"];
     };
     "ui.linenr.selected" = {
-      fg = config.colors.macchiato.mantle.hex;
+      fg = config.colors.macchiato.surface1.hex;
       bg = config.colors.macchiato.sky.hex;
       modifiers = ["bold"];
     };
@@ -101,15 +101,15 @@
     #
     # # "ui.virtual.whitespace" = {bg = config.colors.love.hex;};
     # "ui.virtual.indent-guide" = {fg = config.colors.highlight-high.hex;};
-    "ui.popup" = {bg = config.colors.macchiato.mantle.hex;};
-    "ui.popup.info" = {bg = config.colors.macchiato.mantle.hex;};
-    # "ui.window" = {
-    #   bg = config.colors.base.hex;
-    #   fg = config.colors.base.hex;
-    #   modifiers = ["hidden"];
-    # };
+    "ui.popup" = {bg = config.colors.macchiato.surface1.hex;};
+    "ui.popup.info" = {bg = config.colors.macchiato.surface1.hex;};
+    "ui.window" = {
+      bg = config.colors.macchiato.surface1.hex;
+      fg = config.colors.macchiato.surface1.hex;
+      modifiers = ["hidden"];
+    };
     "ui.help" = {
-      bg = config.colors.macchiato.mantle.hex;
+      bg = config.colors.macchiato.surface1.hex;
       fg = config.colors.subtle.hex;
     };
     #
