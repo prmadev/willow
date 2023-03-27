aedf473e2eafb404dfa7acfb6175d3ef760bc2e1
amirography <amirhossein.alesheikh@gmail.com>
2023-03-27 11:33:59 +0330

    feat(): make quick daily changes

--- home/modules/terminal/tmux.nix
+++ home/modules/terminal/tmux.nix
@@ -8,27 +8,31 @@ with lib; {
   options = {
     tmux.enable = mkEnableOption "tmux";
   };
-  config = mkIf config.tmux.enable {
-    programs.tmux = {
-      enable = true;
-      terminal = "wezterm";
-      historyLimit = 10000;
-      escapeTime = 0;
-      mouse = true;
-      shell = "${pkgs.zsh}/bin/zsh";
-      sensibleOnTop = true;
-      plugins = with pkgs; [
-        tmuxPlugins.open
-        tmuxPlugins.tmux-fzf
-        {
-          plugin = tmuxPlugins.catppuccin;
-          extraConfig = "set -g @catppuccin_flavour 'macchiato'\n" + "set -g @catppuccin_window_tabs_enabled on";
-        }
-        tmuxPlugins.tmux-thumbs
+  config = let
+    nextToggle = "bind-key -T M-p s next-window";
+  in
+    mkIf config.tmux.enable {
+      programs.tmux = {
+        enable = true;
+        terminal = "wezterm";
+        historyLimit = 10000;
+        escapeTime = 0;
+        mouse = true;
+        shell = "${pkgs.zsh}/bin/zsh";
+        sensibleOnTop = true;
+        extraConfig = nextToggle + "\n";
+        plugins = with pkgs; [
+          tmuxPlugins.open
+          tmuxPlugins.tmux-fzf
+          {
+            plugin = tmuxPlugins.catppuccin;
+            extraConfig = "set -g @catppuccin_flavour 'macchiato'\n" + "set -g @catppuccin_window_tabs_enabled on";
+          }
+          tmuxPlugins.tmux-thumbs
+        ];
+      };
+      home.packages = with pkgs; [
+        smug
       ];
     };
-    home.packages = with pkgs; [
-      smug
-    ];
-  };
 }
