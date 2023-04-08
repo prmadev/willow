a7f9a541d0eacad4e32175a01e54ea683d73e496
amirography <amirhossein.alesheikh@gmail.com>
2023-04-08 15:52:34 +0330

    feat(): make quick daily changes

--- home/modules/wm/river/default.nix
+++ home/modules/wm/river/default.nix
@@ -23,5 +23,23 @@ with lib; {
       XKB_DEFAULT_LAYOUT = "us,ir";
       XKB_DEFAULT_OPTIONS = "grp:alt_shift_toggle,caps:escape";
     };
+
+    home.file = {
+      tmuxPicker = {
+        enable = true;
+        executable = true;
+        source = pkgs.writeShellScript "tmux-picker" ''
+          smug_list=$(smug list)
+
+          chosen=$(echo "$smug_list" | rofi -dmenu)
+          if [[ "$smug_list" == *"$chosen"* ]]; then
+              wezterm start smug "$chosen" -a
+            else
+              wezterm start tmux new -s "$chosen"
+          fi
+        '';
+        target = ".local/bin/tmux-picker";
+      };
+    };
   };
 }
