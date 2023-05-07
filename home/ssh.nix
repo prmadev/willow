5dbfd2852e328f0afc72a9092a30335deb7945b9
amirography <amirhossein.alesheikh@gmail.com>
2023-05-07 14:56:43 +0330

    feat(): make quick daily changes

--- home/ssh.nix
+++ home/ssh.nix
@@ -7,6 +7,15 @@ with lib; {
   options.ssh.enable = mkEnableOption "ssh settings";
 
   config = mkIf config.ssh.enable {
-    programs.ssh.enable = true;
+    programs.ssh = {
+      enable = true;
+      matchBlocks = {
+        "git.jafam.ir" = {
+          identityFile = [".ssh/github"];
+          host = "git.jafam.ir";
+          user = "amir.al";
+        };
+      };
+    };
   };
 }
