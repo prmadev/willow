d4d7b9adc969a6543d78ee6778610ea5a42371d5
amirography <amirhossein.alesheikh@gmail.com>
2023-05-07 18:54:40 +0330

    feat(): make quick daily changes

--- home/chromium.nix
+++ home/chromium.nix
@@ -1,15 +1,25 @@
-_: {
-  programs.chromium = {
-    enable = false;
-    extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
-      "dhdgffkkebhmkfjojejmpbldmpobfkfo" # Tampermonkey
-      "cojpndognjdcakkimaloeealehpkljna" # caretTab
-      "gjnmgffpgcigkfipakdijeonkoelhcdh" # rose pine
-      "eimadpbcbfnmbkopoojfekhnkhdbieeh" # dark reader
-      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
-      "padekgcemlokbadohgkifijomclgjgif" # SwitchyOmega
-      "ennpfpdlaclocpomkiablnmbppdnlhoh" # rust search engine
-    ];
+{
+  lib,
+  config,
+  ...
+}:
+with lib; {
+  options.chrome.enable = mkEnableOption "chrome settings";
+
+  config = mkIf config.chrome.enable {
+    programs.chromium = {
+      enable = false;
+      extensions = [
+        "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
+        # "dhdgffkkebhmkfjojejmpbldmpobfkfo" # Tampermonkey
+        "cojpndognjdcakkimaloeealehpkljna" # caretTab
+        # "gjnmgffpgcigkfipakdijeonkoelhcdh" # rose pine
+        "eimadpbcbfnmbkopoojfekhnkhdbieeh" # dark reader
+        "nngceckbapebfimnlniiiahkandclblb" # bitwarden
+        "padekgcemlokbadohgkifijomclgjgif" # SwitchyOmega
+        # "ennpfpdlaclocpomkiablnmbppdnlhoh" # rust search engine
+      ];
+      commandLineArgs = ["--ozone-platform-hint=auto"];
+    };
   };
 }
