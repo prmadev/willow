55d7c04d270c38ef63e8add60c992b1eaf19e98b
prma <me@prma.dev>
2023-12-03 18:29:49 +0330

    chore: update the flake lock definition

--- home/zig.nix
+++ home/zig.nix
@@ -10,9 +10,9 @@ with lib; {
 
   config = mkIf config.zig.enable {
     home.packages = [
-      # pkgs.zigpkg
+      pkgs.zigpkg
       # pkgs.zls
-      # inputs.zls.packages.x86_64-linux.zls
+      inputs.zls.packages.x86_64-linux.zls
     ];
   };
 }
--- system/locale.nix
+++ system/locale.nix
@@ -1,7 +1,6 @@
 _: {
   time.timeZone = "Asia/Tehran";
   i18n.defaultLocale = "en_US.UTF-8";
-  services.localtimed.enable = true;
   console = {
     # font = "Lat2-Terminus16";
     #  keyMap = "us";
