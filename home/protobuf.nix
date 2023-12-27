9f6b227fab9ab037daf530b11cc309c9b13fec75
Perma Alesheikh <me@prma.dev>
2023-12-27 17:40:10 +0330

    add more protoc plugins
    Signed-off-by: Perma Alesheikh <me@prma.dev>

--- home/protobuf.nix
+++ home/protobuf.nix
@@ -8,6 +8,6 @@ with lib; {
   options.protobuf.enable = mkEnableOption "protobuf settings";
 
   config = mkIf config.protobuf.enable {
-    home.packages = with pkgs; [protobuf protoc-gen-rust go-protobuf buf-language-server buf protolint];
+    home.packages = with pkgs; [protobuf protoc-gen-rust protoc-gen-tonic protoc-gen-go-grpc protoc-gen-connect-go protoc-gen-grpc-web protoc-gen-rust-grpc go-protobuf buf-language-server buf protolint];
   };
 }
